6007c6c629e385cd623525aed9b90b61f61f8fcf
= <=>
2023-05-10 20:19:31 +0700

    n

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-wal -i /home/ejix/Pictures/wallpapers/desktop/wallpapers-zh81k0lczz4a1.jpg 
-wal-telegram --wal
+wal -i /home/ejix/desktop/wallpapers-mb761ccn2nua1.jpg
 setxkbmap -option 'caps:super'
-picom -b &
+picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
+# Low battery notifier
+~/.config/qtile/scripts/check_battery.sh & disown
+
+# Start welcome
+eos-welcome & disown
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
